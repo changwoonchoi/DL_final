75bba75de79de7c506e8dc295eb2be308c4a564b
ccw <zzzmaster@snu.ac.kr>
2020-12-21 23:25:47 +0900

    debug logger function

--- final-project-deep-learning-20-pt/utils/log.py
+++ final-project-deep-learning-20-pt/utils/log.py
@@ -10,9 +10,9 @@ def create_logger(log_filename, display=True):
         if display:
             print(text)
         f.write(text + '\n')
-        counter[0] += 1
-        if counter[0] % 10 == 0:
-            f.flush()
-            os.fsync(f.fileno())
+        # counter[0] += 1
+        # if counter[0] % 10 == 0:
+        f.flush()
+        os.fsync(f.fileno())
         # Question: do we need to flush()
     return logger, f.close
